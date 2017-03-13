c07480511c6b696c14d58b56b9e50a30cbc1b657
Matthew R Marino <mm02971@srcs.loc>
2017-03-13 07:59:50 -0700

    all the things

--- functions.js
+++ functions.js
@@ -4,7 +4,7 @@
 * @param {string} start The starting string
 * @param {string} end The ending string
 * @return {string} The string in between
-* @throws (Error} If start or end not found
+* @throws {Error} If start or end not found
 */
 function between(string, start, end) {
     var startAt = string.indexOf(start);
@@ -22,10 +22,12 @@ function between(string, start, end) {
 
     return string.slice(startAt, endAt);
 }
+
 /**
- * Returns an area code from a phone number
+ * Returns an area code from a phone number: (###) ###-####
  * @param   {string} phoneNum The phone number
  * @returns {string} The area code
+ * @throws {Error} If the format is incorrect
  */
 function getAreaCode(phoneNum) {
 
@@ -34,34 +36,15 @@ function getAreaCode(phoneNum) {
     try {
         areaCode = between(phoneNum, "(", ")");
         areaCode = areaCode.trim();
-        if(areaCode.length == 3 && Number(areaCode)){
+        if (areaCode.length == 3 && Number(areaCode)) {
             return areaCode;
-        } else{
+        } else {
             throw new Error("Invalid area code: " + areaCode);
         }
     } catch (error) {
         throw new Error("Invalid phone number: " + error.message);
-        console.log(error.message);
-        //return undefined;
     }
 }
-
-function getLineCode(phoneNum){
-    var lineCode;
-
-    try{
-        lineCode = between(phoneNum, "-","");
-        lineCode = lineCode.trim();
-        if(lineCode.length = 4 && Number(lineCode)){
-            return lineCode;
-        } else{
-            throw new Error("Invalid line code: " + lineCode);
-        }
-    }catch (error) {
-            throw new Error("Invalid phone number: " + error.message);
-
-        }
- }
 
 /**
  * Displays the area code for an inputted phone number
@@ -85,17 +68,40 @@ function displayAreaCode(inputId, outputId) {
 }
 
 /**
+ * Returns a line code from a phone number: (###) ###-####
+ * @param   {string} phoneNum The phone number
+ * @returns {string} The line code
+ * @throws {Error} If the format is incorrect
+ */
+function getLineCode(phoneNum) {
+
+    var lineCode;
+
+    try {
+        lineCode = phoneNum.slice(10);
+        if (lineCode.length == 4 && Number(lineCode)) {
+            return lineCode;
+        } else {
+            throw new Error("Invalid line code: " + lineCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+/**
  * Displays the line code for an inputted phone number
  * @param {string} inputId  The element id for the text box
  * @param {string} outputId The element id of message div
  */
 function displayLineCode(inputId, outputId) {
+
     var outputText = "";
-    var phoneNum = document.getElementById(inputId).value;
+    var phoneNum = document.getElementById(outputId).value;
 
     // Now try to get the code
     try {
-        var lineCode = getLineCode(phoneNum);
+        var areaCode = getLineCode(phoneNum);
         outputText = "Your line code is " + lineCode;
     } catch (error) {
         console.log(error.message);
@@ -104,3 +110,81 @@ function displayLineCode(inputId, outputId) {
 
     document.getElementById(outputId).innerHTML = outputText;
 }
+/**
+ * Returns a CO code from a phone number: (###) ###-####
+ * @param   {string} phoneNum The phone number
+ * @returns {string} The CO code
+ * @throws {Error} If the format is incorrect
+ */
+function getCoCode(phoneNum) {
+
+    var coCode;
+
+    try {
+        coCode = between(phoneNum, ") ", "-");
+        if (coCode.length == 3 && Number(coCode)) {
+            return coCode;
+        } else {
+            throw new Error("Invalid CO code: " + coCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+/**
+ * Displays the CO code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayCoCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var coCode = getCoCode(phoneNum);
+        outputText = "Your CO code is " + coCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
+}
+/*
+function validPhone(phoneNum) {
+	// check and remove parenthesis
+	if (phoneNum.indexOf('(') != 0 && phoneNum.indexOf(')') != 4) {
+		console.log("Missing parenthesis.");
+		return false;
+	}
+	else {
+		//remove the parenthesis
+		phoneNum = phoneNum.replace(')', '');
+		phoneNum = phoneNum.replace('(', '');
+		console.log(phoneNum);
+	}
+	// check to see if the - is in the correct place and the other digits are numbers
+	var cell = phoneNum.split('-');
+	console.log(cell);
+	if (Number(cell[1]) && Number(cell[0]) && phoneNum.charAt(6) == '-' && phoneNum.length == 11) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function disPhoneNum(inputId, outputId) {
+	var input = document.getElementById(inputId).value;
+	var outputText = "";
+	if (validPhone(input) == true) {
+		outputText = "The number " + input + " is a actual phone number.";
+	}
+	else {
+		outputText = "The number " + input + " is not a phone number.";
+	}
+	document.getElementById(outputId).innerHTML = outputText;
+}
+*/
--- tests.js
+++ tests.js
@@ -5,10 +5,28 @@ QUnit.test("Test the getAreaCode function.", function (assert) {
 });
 QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
     assert.throws( function() {
-        getAreCode("(415) 312-8774");
+        getAreaCode("(415) 555-5555");
     }, "Missing '('. An error should have been thrown." );
+});
 
+QUnit.test("Test the getLineCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getLineCode(num);
+    assert.deepEqual(result, "5555", "Valid line code test passed.");
+});
+QUnit.test( "Errors thrown for getLineCode", function( assert ) {
     assert.throws( function() {
-        getAreCode("(415) 312-8774");
-    }, "Missing ')'. An error should have been thrown." );
+        getLineCode("(415) 555-5555");
+    }, "Missing '('. An error should have been thrown." );
+});
+
+QUnit.test("Test the getCoCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getCoCode(num);
+    assert.deepEqual(result, "555", "Valid co code test passed.");
+});
+QUnit.test( "Errors thrown for getCoCode", function( assert ) {
+    assert.throws( function() {
+        getCoCode("(415) 555-5555");
+    }, "Missing '('. An error should have been thrown." );
 });
